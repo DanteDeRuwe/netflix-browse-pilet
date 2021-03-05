62a7c9ef4e62c847bd78124a2a51105f00b88368
Dante De Ruwe <dantederuwe@gmail.com>
2021-03-05 11:39:40 +0100

    Transform pilet into "browse" pilet

---
+++ src/components/Browse.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import Hero from './Hero';
+import TitleList from './TitleList';
+
+const Browse: React.FC = () => (
+  <div>
+    <Hero />
+    <TitleList title="Top TV picks for Jack" url="discover/tv?sort_by=popularity.desc&page=1" />
+    <TitleList title="Trending now" url="discover/movie?sort_by=popularity.desc&page=1" />
+    <TitleList title="Most watched in Horror" url="genre/27/movies?sort_by=popularity.desc&page=1" />
+    <TitleList title="Sci-Fi greats" url="genre/878/movies?sort_by=popularity.desc&page=1" />
+    <TitleList title="Comedy magic" url="genre/35/movies?sort_by=popularity.desc&page=1" />
+  </div>
+);
+
+export default Browse;
